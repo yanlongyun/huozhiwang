38a7480cf60da4d9c82dbf579ad0e947c42db159
yanlongyun <13226404626@163.com>
2020-07-11 22:47:16 +0800

    初始化项目

---
+++ src/main/java/com/zrzhen/huozhiwang/HuozhiwangApplication.java
@@ -0,0 +1,13 @@
+package com.zrzhen.huozhiwang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuozhiwangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuozhiwangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zrzhen/huozhiwang/HuozhiwangApplicationTests.java
@@ -0,0 +1,9 @@
+package com.zrzhen.huozhiwang;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuozhiwangApplicationTests {
+
+
+}
